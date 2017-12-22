201a2894e7a06b3b4c799ac0a2d25ae5a0165fdd
Sitati Kituyi <sitati.kituyi@googlemail.com>
2017-12-22 14:09:26 +0300

    WIP

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
 var vorpal = require('vorpal')();
+const chalk = vorpal.chalk;
+var jsonfile = require('jsonfile');
+var _ = require('lodash');
+
+var file = 'kufanya.json'
+var data;
+
+jsonfile.readFile(file, function(err, obj) {
+  data = obj;
+})
+
+function persist() {
+  jsonfile.writeFile(file, data, function (err) {
+  });
+}
+
+function addTodo(newTodo) {
+  if(typeof(data) === 'undefined') {
+    data = { todos: [] }
+  }
+  data.todos.push({
+    text: newTodo,
+    date: new Date().getTime(),
+    done: false
+  });
+  persist();
+}
 
 function interactiveAdd(v) {
   v.prompt({
@@ -8,6 +35,7 @@ function interactiveAdd(v) {
   },
   function(result){
     if (result.newTodo) {
+      addTodo(result.newTodo);
       interactiveAdd(v);
     }
   });
@@ -22,5 +50,41 @@ vorpal
   });
 
 vorpal
-.delimiter('>>')
-.show();
+  .command('list', 'List todos')
+  .option('-a, --all')
+  .action(function(args, callback) {
+    var midnight = new Date();
+    midnight.setHours(0,0,0,0);
+    var undone = _.filter(data.todos, function(o) { return !o.done });
+    var today = _.filter(data.todos, function(o) {
+      return o.date > midnight;
+    });
+    this.log("Today")
+    reportTodos(_.intersection(today, undone), "Todo", false, this);
+    reportTodos(_.difference(today, undone), "Done", true, this);
+    this.log("\n");
+
+    callback();
+  });
+
+function reportTodos(sublist, label, areDone, v) {
+  if(sublist.length === 0) {
+    return;
+  }
+  var chalkingFunction;
+  if(areDone) {
+    chalkingFunction = chalk.grey;
+  }
+  else {
+    chalkingFunction = chalk.cyan;
+  }
+  v.log(`  ${chalkingFunction(label)}`);
+  _.each(sublist, function(entry) {
+    v.log(chalkingFunction(`  [${areDone ? 'x' : ' '}] ${entry.text}`));
+  })
+  v.log(chalkingFunction(''));
+}
+
+vorpal
+  .delimiter('>>')
+  .show();
